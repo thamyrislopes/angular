13aaf708583e8b317cbd52dfca3c77a030c31538
Thamyris Lopes <101022477+thamyrislopes@users.noreply.github.com>
2024-03-02 18:12:08 -0300

    update

---
+++ ex02/main.js
@@ -0,0 +1,46 @@
+
+
+var d = new Date();
+console.log(d);
+console.log(d.getDay());
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+/*
+var count = 0;
+while (count <=5){
+    console.log (count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual é a sua idade?");
+if (idade >= 18)
+    alert ("Maior de idade.")
+else
+    alert ("Menor de idade.")
+*/
+
+/*
+var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+*/
+
+/*
+var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta);
+*/
+
+//var lista = ["maçã", "pêra", "morango"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.reverse());
+//console.log(lista);
+//console.log(lista.toString())
+//console.log(lista.join(" - "));
